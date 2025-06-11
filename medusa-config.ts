614122b335b7dd79dbc0cd0c2adfe1a05819ff6e
vuquyet808 <vuquyet808@gmail.com>
2025-06-11 23:17:26 +0700

    fix cord

--- medusa-config.ts
+++ medusa-config.ts
@@ -10,7 +10,8 @@ module.exports = defineConfig({
     http: {
       storeCors: process.env.STORE_CORS || "http://localhost:8000",
       adminCors:
-        process.env.ADMIN_CORS || "https://thomxiu-store-be.vercel.app",
+        "http://localhost:5173,http://localhost:9000,https://docs.medusajs.com,https://thomxiu-store-be.vercel.app",
+
       authCors: process.env.AUTH_CORS || "http://localhost:5173",
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
